a491d21c8adabc380280ae1be6fa119f52b374cd
Joven Peralta <jperalta@lcs.on.ca>
2019-10-23 20:39:45 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Peralta, Joven on 2019-10-23.
+//  Copyright © 2019 Peralta, Joven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
